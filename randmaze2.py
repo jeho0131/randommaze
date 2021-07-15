04c75636b73806919e42b145101b2e759b9680fe
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-07-15 16:25:27 +0900

    Add files via upload

--- randmaze2.py
+++ randmaze2.py
@@ -1,8 +1,11 @@
-import pygame, sys, random
-from pygame.locals import * 
+import pygame, sys, random, time
+from pygame.locals import *
+
+xm = 400
+ym = 400
 
 pygame.init()
-size = [600, 600]
+size = [xm+100, ym + 100]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Maze")
 clock = pygame.time.Clock()
@@ -14,14 +17,15 @@ wall_image = pygame.transform.scale(big_wall_image, (10,10))
 big_player_image = pygame.image.load("player.png")
 player_image = pygame.transform.scale(big_player_image, (10,10))
 
+font = pygame.font.Font(None, 40)
 endfont = pygame.font.Font(None, 60)
 
 g = 0
-xm = 500
-ym = 500
-px = 50
-py = 50
+px = int(xm / 2) + 50
+py = int(ym / 2) + 50
 gxy = [0,0]
+ml = []
+mazechange = time.time()
 
 def r(l):
     n = random.randint(0,3)
@@ -51,17 +55,26 @@ def Maze(x, y, s):
     no = noc(x, y, s)
     
     if no.count(1) >= 4:
-        if g == 0:
-          if y == ym:
-              s.add((x, y+10))
-              gxy[0] = x
-              gxy[1] = y+20
-              g += 1
-          elif x == xm:
-              s.add((x+10, y))
-              gxy[0] = x+20
-              gxy[1] = y
-              g += 1
+        if y == ym and g == 0:
+            s.add((x, y+10))
+            gxy[0] = x
+            gxy[1] = y+20
+            g += 1
+        if x == xm and g == 0:
+            s.add((x+10, y))
+            gxy[0] = x+20
+            gxy[1] = y
+            g += 1
+        if y == 0 and g == 0:
+            s.add((x, y-10))
+            gxy[0] = x
+            gxy[1] = y-20
+            g += 1
+        if x == 0 and g == 0:
+            s.add((x-10, y))
+            gxy[0] = x-20
+            gxy[1] = y
+            g += 1
           
         return s
       
@@ -134,27 +147,45 @@ def drawMaze(l):
 def movePlayer():
     global px, py, gameover
     
-    if (pressed_keys[K_w] or pressed_keys[K_UP]) and (px-50, py-60) in l:
+    if (pressed_keys[K_w] or pressed_keys[K_UP]) and (px-50, py-60) in ml:
         py -= 10
-    elif (pressed_keys[K_a] or pressed_keys[K_LEFT]) and (px-60, py-50) in l:
+    elif (pressed_keys[K_a] or pressed_keys[K_LEFT]) and (px-60, py-50) in ml:
         px -= 10
-    elif (pressed_keys[K_s] or pressed_keys[K_DOWN]) and (px-50, py-40) in l:
+    elif (pressed_keys[K_s] or pressed_keys[K_DOWN]) and (px-50, py-40) in ml:
         py += 10
-    elif (pressed_keys[K_d] or pressed_keys[K_RIGHT]) and (px-40, py-50) in l:
+    elif (pressed_keys[K_d] or pressed_keys[K_RIGHT]) and (px-40, py-50) in ml:
         px += 10
         
-    if (gxy[0] == px-50 and gxy[1] == py-40) or (gxy[0] == px-40 and gxy[1] == py-50):
+    if ((gxy[0] == px-50 and gxy[1] == py-40) or (gxy[0] == px-40 and gxy[1] == py-50)) or ((gxy[0] == px-60 and gxy[1] == py-50) or (gxy[0] == px-50 and gxy[1] == py - 60)):
         gameover = False
+
+def makeMaze():
+    xy = set()
+    xy.add((int(xm / 2),int(ym/2)))
+    a = Maze(int(xm / 2),int(ym/2),xy)
+    l = []
+    l = list(a)
+    l.sort()
+    return l
         
 def drawPlayer():
     screen.blit(player_image, (px, py))
 
-xy = set()
-xy.add((0,0))
-a = Maze(0,0,xy)
-l = []
-l = list(a)
-l.sort()
+def timec():
+    global ml, mazechange, g
+    t = 30 - (time.time() - mazechange)
+    txt = font.render(str(int(t)), True, (255,0,0))
+    tx = int(size[0] / 2 - txt.get_width() / 2)
+    ty = int(size[1] * 1 / 24 - txt.get_height() / 2)
+
+    screen.blit(txt, (tx,ty))
+    
+    if time.time() - mazechange > 30:
+        g = 0
+        ml = makeMaze()
+        mazechange = time.time()
+
+ml = makeMaze()
 
 while gameover:
     clock.tick(10)
@@ -169,9 +200,10 @@ while gameover:
 
     movePlayer()
     drawPlayer()
-    drawMaze(l)
+    drawMaze(ml)
+    timec()
     pygame.display.update()
-    
+
 screen.fill(color)
     
 txt = endfont.render("Congratulation!", True, (0,255,0))
